899c5c780393500a951a3271256882a7dce05359
ArielAio <arielaio@hotmail.com>
2024-08-20 23:39:16 -0300

    feature: melhorando loading

--- lib/AuthRoute.jsx
+++ lib/AuthRoute.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import { Oval } from 'react-loader-spinner'; 
 
 const AuthRoute = ({ children }) => {
     const [loading, setLoading] = useState(true);
@@ -22,7 +23,19 @@ const AuthRoute = ({ children }) => {
     }, [router]);
 
     if (loading) {
-        return <p>Carregando...</p>;
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'white' }}>
+                <Oval
+                    height={80}
+                    width={80}
+                    color="#4fa94d"
+                    ariaLabel="oval-loading"
+                    secondaryColor="#4fa94d"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                />
+            </div>
+        );
     }
 
     return user ? children : null;
--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { db } from "../../lib/firebase";
 import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import AuthRoute from "../../lib/AuthRoute";
-
+import { Oval } from "react-loader-spinner";
 
 export default function Listar() {
     const [jobPosts, setJobPosts] = useState([]);
@@ -42,52 +42,67 @@ export default function Listar() {
         }
     };
 
-    if (loading) return <p className="text-center text-gray-500">Carregando...</p>;
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center min-h-screen bg-white">
+                <Oval
+                    height={80}
+                    width={80}
+                    color="#4fa94d"
+                    visible={true}
+                    ariaLabel="oval-loading"
+                    secondaryColor="#4fa94d"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                />
+            </div>
+        );
+    }
 
-    if (error) return <p className="text-center text-red-500">{error}</p>;
+    if (error) {
+        return <p className="text-center text-red-500">{error}</p>;
+    }
 
     return (
-    <AuthRoute>
-
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
-            <div className="bg-white shadow-lg rounded-lg p-6 md:p-8 w-full max-w-4xl"> {/* Ajuste de padding e largura */}
-                <h1 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6 text-center animate-fadeIn">
-                    Listagem de Estágios/Trabalhos
-                </h1>
-                {jobPosts.length === 0 ? (
-                    <p className="text-center text-gray-500">Nenhum estágio/trabalho cadastrado.</p>
-                ) : (
-                    <ul className="space-y-4">
-                        {jobPosts.map((post) => (
-                            <li
-                                key={post.id}
-                                className="border border-gray-300 rounded-lg p-4 shadow-sm transform transition-transform duration-500 hover:scale-105 hover:shadow-xl animate-fadeIn"
-                            >
-                                <h2 className="text-lg md:text-xl font-semibold text-gray-800">{post.company_name}</h2>
-                                <p className="text-gray-600">Tipo: {post.type === "internship" ? "Estágio" : "Trabalho"}</p>
-                                <p className="text-gray-600">Requisitos: {post.requirements}</p>
-                                <p className="text-gray-600">Salário/Bolsa: {post.salary_or_stipend}</p>
-                                <div className="mt-4 flex flex-col sm:flex-row sm:space-x-4 space-y-2 sm:space-y-0"> {/* Flex column for small screens */}
-                                    <button
-                                        onClick={() => handleEdit(post.id)}
-                                        className="py-2 px-4 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition-transform duration-300 transform hover:scale-105"
-                                    >
-                                        Editar
-                                    </button>
-                                    <button
-                                        onClick={() => handleDelete(post.id)}
-                                        className="py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-transform duration-300 transform hover:scale-105"
-                                    >
-                                        Excluir
-                                    </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
-        </main>
-    </AuthRoute>
-
+        <AuthRoute>
+            <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
+                <div className="bg-white shadow-lg rounded-lg p-6 md:p-8 w-full max-w-4xl">
+                    <h1 className="text-2xl md:text-3xl font-bold text-gray-800 mb-6 text-center animate-fadeIn">
+                        Listagem de Estágios/Trabalhos
+                    </h1>
+                    {jobPosts.length === 0 ? (
+                        <p className="text-center text-gray-500">Nenhum estágio/trabalho cadastrado.</p>
+                    ) : (
+                        <ul className="space-y-4">
+                            {jobPosts.map((post) => (
+                                <li
+                                    key={post.id}
+                                    className="border border-gray-300 rounded-lg p-4 shadow-sm transform transition-transform duration-500 hover:scale-105 hover:shadow-xl animate-fadeIn"
+                                >
+                                    <h2 className="text-lg md:text-xl font-semibold text-gray-800">{post.company_name}</h2>
+                                    <p className="text-gray-600">Tipo: {post.type === "internship" ? "Estágio" : "Trabalho"}</p>
+                                    <p className="text-gray-600">Requisitos: {post.requirements}</p>
+                                    <p className="text-gray-600">Salário/Bolsa: {post.salary_or_stipend}</p>
+                                    <div className="mt-4 flex flex-col sm:flex-row sm:space-x-4 space-y-2 sm:space-y-0">
+                                        <button
+                                            onClick={() => handleEdit(post.id)}
+                                            className="py-2 px-4 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition-transform duration-300 transform hover:scale-105"
+                                        >
+                                            Editar
+                                        </button>
+                                        <button
+                                            onClick={() => handleDelete(post.id)}
+                                            className="py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-transform duration-300 transform hover:scale-105"
+                                        >
+                                            Excluir
+                                        </button>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </main>
+        </AuthRoute>
     );
 }
