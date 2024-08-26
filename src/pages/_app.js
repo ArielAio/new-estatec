bef5fb85fbe6df9e7573278add797ff5eacc3e1d
ArielAio <arielaio@hotmail.com>
2024-08-26 16:50:57 -0300

    featute-test: testando chatbot

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css';
 import Layout from '../../components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <script src="//code.tidio.co/ifdcvczkyhkwdeyum9kijyd9o9tkdccl.js" async></script>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
